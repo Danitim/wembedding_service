04d28dc618e94d316a521adac1f078cdf70c52d3
Danitim <danitim4@gmail.com>
2026-03-18 14:33:00 +0300

    Added ModernBERT model

--- wembeddings/wembeddings.py
+++ wembeddings/wembeddings.py
@@ -25,6 +25,7 @@ class WEmbeddings:
         # Key: model name. Value: transformer model name, layer start, layer end.
         "bert-base-multilingual-uncased-last4": ("bert-base-multilingual-uncased", -4, None),
         "rubert-conversational-last4": ("DeepPavlov/rubert-base-cased-conversational", -4, None),
+        "modernbert-base-last4": ("answerdotai/ModernBERT-base", -4, None),
         "robeczech-base-last4": ("ufal/robeczech-base", -4, None),
         "xlm-roberta-base-last4": ("xlm-roberta-base", -4, None),
     }
